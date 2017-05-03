22fbfcdaf9f20a9de73be0455e0931392606be07
dwleonhardt <dwleonhardt@gmail.com>
2017-05-03 17:17:34 -0600

    tried adding code to .bot

--- src/bot.js
+++ src/bot.js
@@ -1,33 +1,55 @@
+//
+// 'use strict'
+//
+// const slack = require('slack')
+// const _ = require('lodash')
+// const config = require('./config')
+//
+// let bot = slack.rtm.client()
+//
+// bot.started((payload) => {
+//   this.self = payload.self;
+// });
+//
+// bot.message((msg) => {
+//   if (!msg.user) return
+//   if (!_.includes(msg.text.match(/<@([A-Z0-9])+>/igm), `<@${this.self.id}>`)) return
+//
+//   slack.chat.postMessage({
+//     token: config('SLACK_TOKEN'),
+//     icon_emoji: config('ICON_EMOJI'),
+//     channel: msg.channel,
+//     username: 'Starbot',
+//     text: `beep boop: I hear you loud and clear!"`
+//   }, (err, data) => {
+//     if (err) throw err
+//
+//     let txt = _.truncate(data.message.text)
+//
+//     console.log(`🤖  beep boop: I responded with "${txt}"`)
+//   })
+// })
 
-'use strict'
+require('dotenv').config();
+var Bot = require('slackbots');
 
-const slack = require('slack')
-const _ = require('lodash')
-const config = require('./config')
+var settings = {
+    token: process.env.TOKEN,
+    name: 'sleepy'
+};
+var bot = new Bot(settings);
 
-let bot = slack.rtm.client()
-
-bot.started((payload) => {
-  this.self = payload.self
-})
-
-bot.message((msg) => {
-  if (!msg.user) return
-  if (!_.includes(msg.text.match(/<@([A-Z0-9])+>/igm), `<@${this.self.id}>`)) return
-
-  slack.chat.postMessage({
-    token: config('SLACK_TOKEN'),
-    icon_emoji: config('ICON_EMOJI'),
-    channel: msg.channel,
-    username: 'Starbot',
-    text: `beep boop: I hear you loud and clear!"`
-  }, (err, data) => {
-    if (err) throw err
-
-    let txt = _.truncate(data.message.text)
-
-    console.log(`🤖  beep boop: I responded with "${txt}"`)
-  })
-})
+bot.on('start', function() {
+    bot.postMessageToChannel('general', 'Hello channel!');
+    // bot.postMessageToUser('dwleonhardt', 'whatever');
+    // bot.postMessageToGroup('some-private-group', 'hello group chat!');
+});
+bot.on('message', function(data) {
+    // all ingoing events https://api.slack.com/rtm
+    console.log(data);
+    if (data.text == 'sleep') {
+    bot.postMessageToUser('dwleonhardt', 'Goodnight!');
+    }
+});
 
 module.exports = bot
